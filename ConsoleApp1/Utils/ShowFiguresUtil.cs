61d7e630faaac0315c81061230a0b47392dcbf11
Александр <iliynsd@icloud.com>
2022-02-21 18:47:57 +0300

    add class to show some figures

---
+++ ConsoleApp1/Utils/ShowFiguresUtil.cs
@@ -0,0 +1,109 @@
+using System;
+
+namespace ConsoleApp1.Utils
+{
+    public static class ShowFiguresUtil
+    {
+        public static void ShowRectangle((int,int) sides)
+        {
+            for (int i = 0; i < sides.Item2; i++)
+            {
+                for (int j = 0; j < sides.Item1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ShowTriangle(int cathet)
+        {
+            for (int i = 0; i < cathet; i++)
+            {
+                for (int j = 0; j < i+1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ShowTurnedTriangle(int cathet)
+        {
+            for (int i = 0; i < cathet; i++)
+            {
+                for (int j = cathet; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i+1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ShowTriangleInverted(int cathet)
+        {
+            for (int i = 0; i < cathet; i++)
+            {
+                for (int j = cathet; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ShowTriangleInvertedAndTurned(int cathet)
+        {
+            for (int i = 0; i < cathet; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = cathet; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        public static void ShowRhombus(int side)
+        {
+            int center = side / 2;
+            for(int i = 0; i < side; i++)
+            {
+                for(int j = 0; j < side; j++)
+                {
+                    if(i <= center)
+                    {
+                        if (j >= center - i && j <= center + i)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    else
+                    {
+                        if (j >= center + i - side + 1 && j <= center - i + side - 1)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
